43fc725ffa87a4c14129f8686b1c227f481134e1
George Markosov <george.markosov@deliveryweb.ru>
2019-07-15 01:19:48 +0300

    Added components for displaying recipes and shopping list; Added header component with navbar; Added ingredient and recipe models;

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from 'app/common/models/recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('First Test Recipe', 'Simple description', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('Second Test Recipe', 'Simple description', 'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_960_720.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
